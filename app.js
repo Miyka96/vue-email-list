24087b434496fda7bfee2220cc0d16868e955d5c
Miyka96 <micaelaseminara@outlook.it>
2022-03-07 02:58:02 +0100

    finished

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ const app = new Vue ({
     el: ('#root'),
     data: {
         emails: [],
+        emailsNumber: 10,
+    },
+    created(){
+        for (let i=0; i<this.emailsNumber; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (res => {
+                if(res.data.success) {
+                    this.emails.push( res.data.response );
+                 }
+            }
+            ))
+        }
     }
 })
